c4d4a793b1ec2943584c183bf137615465b519c4
Shyshot <103759760+Shyshot@users.noreply.github.com>
2024-02-25 12:49:01 +0530

    Create file.js

---
+++ portfolio/file.js
@@ -0,0 +1,46 @@
+ const dropzoneBox = document.getElementsByClassName("dropzone-box")[0];
+ const inputFiles = document.querySelectorAll("dropzone-area input[type='file']");
+ const inputElement= inputFiles[0];
+
+ const dropZoneElement = inputElement.closest(".dropzonearea");
+ inputElement.addEventListener("change",(e)=>{
+     if (inputElement.files.length){
+             updateDropzoneFileList(
+                dropZoneElement, inputElement.files[0]
+             );
+     }
+ });
+dropZoneElement.addEventListener("dragover",(e)=>{
+    e.preventDefault();
+    dropZoneElement.classList.add("dropzone--over");
+});
+["dragleave","dragend"].forEach((type)=>{
+    dropZoneElement.addEventListener(type, (e)=>{
+        dropZoneElement.classList.remove("dropzone--over");
+    });
+});
+dropZoneElement.addEventListener("drop", (e)=>{
+    e.preventDefault();
+
+    if(e.dataTransfer.files.length){
+        inputElement.files=e.dataTransfer.files;
+            updateDropzoneFileList(dropZoneElement, e.dataTransfer.files[0]);
+    }
+    dropZoneElement.classList.remove("dropzone--over");
+});
+ const updateDropzoneFileList = (dropZoneElement , file)=> {
+     let dropzoneFileMessage = dropZoneElement.querySelector(".file-info");
+ dropzoneFileMessage.innerHtml = '${file.name}, ${file.size}bytes' ;   
+ };
+ dropzoneBox.addEventListener("reset" , (e)=>{
+    let dropzoneFileMessage = dropZoneElement.querySelector(".file-info");
+
+    dropzoneFileMessage.innerHTML = 'No files selected';
+ });
+
+ dropzoneBox.addEventListener("submit", (e)=>{
+     e.preventDefault();
+     const myFiled = document.getElementById("upload-file");
+      console.log(myFiled.files[0]);
+ });
+
